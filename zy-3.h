8143acd99032dbcda9324109695b541aa9ef779c
yjwchj <61540228+yjwchj@users.noreply.github.com>
2020-03-04 20:09:00 +0800

    Add files via upload

--- zy-3.h
+++ zy-3.h
@@ -1,30 +1,32 @@
 
 #include<stduo.h>
+#include<stdlib.h>
+#define LEN sizeof(struct province)
 int main()
 {
-	struct province
+	struct province  //建立结构体
 	{int s;
 	int d;
 	int c;
 	int w;
 	int k;
-	struct province *next;
+	struct province *next;  //next是指针变量，指向结构体
 	};
-char choose;
+char choose;  //使用者选择身份
   switch(choose)
-  {case'A':{int number;
+  {case'A':{int number;  //管理员身份
             switch(number)
-			{case'1':pt=creat();
-			case'2':void addnode(struct province *p);
+			{case'1':pt=creat();  //调用开辟内存的链表的函数
+			case'2':void addnode(struct province *p);  //调用增加数据的函数
 			case'3':
 
 
 
 
 
-  case'B':{int number;
+  case'B':{int number;  //普通用户身份
 	       switch(number)
-		   {case'1':print(head);break;
+		   {case'1':print(head);break;  //调用输出数据函数
 		   case'2':
 		   case'3':
 		   case'4':
@@ -41,23 +43,23 @@ char choose;
 	  
 	  
 		  }break;
-degault:printf("enter choose error!\n);
+degault:printf("enter choose error!\n);  //错误提示
   }
 	return 0;
 }
 
-struct province * creat()
+struct province * creat(void)  //定义函数，此函数返回一个指向链表头的指针
 {struct province *head;
 struct province *p1,*p2;
 int n=0;
-p1=p2=(struct province *) mallo(LEN);
-scanf("%d",&p1->s);
+p1=p2=(struct province *) mallo(LEN);  //开辟一个新单元
+scanf("%d",&p1->s);  //输入第一个省的数据
 scanf("%d",&p1->d);
 scanf("%d",&p1->c);
 scanf("%d",&p1->w);
 scanf("%d",&p1->k);
 head=NULL;
-while(p1->num!=0);
+while(p1->num!=0); //若指向的不是空表，则继续循环
 {n++;
  if(n==1)head=p1;
  else p2->nest=p1;
@@ -73,32 +75,36 @@ p2->nest=NULL;
 return(head);
 }
 
-void print(struct province *head)
+void print(struct province *head)  //输出函数
 {struct province *p;
-p=head;
-if(head!=NULL)
+p=head; //头指针head
+if(head!=NULL)  //头指针指向不为空表则循环
   do
-  {printf("%d",&p1->s);
+  {printf("%d",&p1->s);  //输出数据
    printf("%d",&p1->d);
    printf("%d",&p1->c);
    printf("%d",&p1->w);
    printf("%d",&p1->k);
-   p=p->next;
+   p=p->next;  //将下一个结点的地址放到前一个结点的成员next中
   }
-  while(p!==NULL);
+  while(p!==NULL);  //直到指针指向空表
 }
 
-void addnode(struct province *p)
-{struct province *pnew=NULL;
-pnew=(struct province *)malloc(LEN);
+void addnode(struct province *p)  //定义增加数据的函数
+{int n,i;
+scanf("%d",&n);
+	struct province *pnew=NULL;  //定义新的指针
 printf("请添加新的数据\n");
+for(i=0;i<n;i++)
+{pnew=(struct province *)malloc(LEN);  //开辟新结点
 scanf("%d",&p1->s);
 scanf("%d",&p1->d);
 scanf("%d",&p1->c);
 scanf("%d",&p1->w);
 scanf("%d",&p1->k);
-pnew->next=NULL;
+pnew->next=NULL;  
 p->next=pnew;
+}
 }
 
 
