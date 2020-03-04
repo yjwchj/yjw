f8052af4c4e5da736f3bfea47219493c944d63bf
yjwchj <61540228+yjwchj@users.noreply.github.com>
2020-03-04 12:22:56 +0800

    Add files via upload

---
+++ zuoye.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+	struct student
+	{int num;
+	char name[20];
+	float score[4];
+	}stu[];
+void input(struct student stu[])
+{int i;
+for(i=0,i<31.i++)
+{scanf("%d%s%f%f%f",&stu[i].num,stu[i].name,&stu[i].score[0],&stu[i].score[1],&stu[i].score[2],&stu[i].score[3]);
+}
+
+
