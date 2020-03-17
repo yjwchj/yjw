d496be346f4fc7b1cca99aebc64be2a955e75f21
yjwchj <61540228+yjwchj@users.noreply.github.com>
2020-03-17 18:22:51 +0800

    Add files via upload

---
+++ list2.h
@@ -0,0 +1,7 @@
+void matrixadd(double (*a)[200],double (*b)[200],int n,int m);//j矩阵的加法运算函数 
+void matrixsubtract(double (*a)[200],double (*b)[200],int n,int m);//矩阵的减法运算函数 
+void matrixmultiply(double (*a)[200],int n,int m);//矩阵的数乘运算函数  
+void matrixtransposition(double (*a)[200],int n,int m);//矩阵的转置运算函数
+void matrixinverse(double (*a)[200],int n,int m);//矩阵的求逆运算函数 
+void menu();//菜单函数 
+
