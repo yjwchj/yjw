5893040ee081c39be31aa20b10b4c66060b599d5
yjwchj <61540228+yjwchj@users.noreply.github.com>
2020-03-19 02:33:00 +0800

    Add files via upload

--- list2.h
+++ list2.h
@@ -1,7 +1,7 @@
-void matrixadd(double (*a)[200],double (*b)[200],int n,int m);//j矩阵的加法运算函数 
-void matrixsubtract(double (*a)[200],double (*b)[200],int n,int m);//矩阵的减法运算函数 
-void matrixmultiply(double (*a)[200],int n,int m);//矩阵的数乘运算函数  
-void matrixtransposition(double (*a)[200],int n,int m);//矩阵的转置运算函数
-void matrixinverse(double (*a)[200],int n,int m);//矩阵的求逆运算函数 
+void matrixadd(int n,int m,double (*p1)[m],double (*p2)[m]);//j矩阵的加法运算函数 
+void matrixsubtract(int n,int m,double (*p1)[m],double (*p2)[m]);//矩阵的减法运算函数 
+void matrixmultiply(int n,int m,double (*p1)[m]);//矩阵的数乘运算函数  
+void matrixtransposition(int n,int m,double (*p1)[m]);//矩阵的转置运算函数
+void matrixinverse(int n,int m,double (*p1)[m]);//矩阵的求逆运算函数 
 void menu();//菜单函数 
-
+void input(int n,int m,double (*p1)[m],double (*p2)[m]); 
--- main2.c
+++ main2.c
@@ -7,65 +7,25 @@
 
 int main(int argc, char *argv[])
 {
-	printf("请输入矩阵行数和列数：\n");     //请输入目标矩阵 
-	int n, m;                                
-	scanf("%d,%d",&n,&m);                  //定义行数和列数 
-	int i, j;
-	printf("请输入目标矩阵：\n");
-	double a[200][200];
-	for(i=0;i<n;i++)
-	   for(j=0;j<m;j++)
-	       {
-		   scanf("%lf",&a[i][j]);          //输入矩阵 
-		   }
-	for(i=0;i<m;i++)
-	   {
-	     if(i%m==0)                       //换行使其是矩阵的形式 
-	      printf("\n");
-	   }
-	printf("请输入矩阵的行数和列数(如果不需要，请输入和上一个矩阵相同的行数和列数)：\n");     //输入需要的第二个矩阵 
-	int k, g;
-	scanf("%d,%d",&k,&g);                 //定义行数和列数 
-	printf("请输入目标矩阵（如果不需要，请都输入0）：\n");
-	double b[200][200];
-	for(i=0;i<k;i++)
-	   for(j=0;j<g;j++)
-	       {
-		    scanf("%lf",&b[i][j]);      //输入目标矩阵 
-		   }
-	for(i=0;i<g;i++)
-	   {
-	    if(i%g==0)                     //换行 
-	      printf("\n");
-       }
-    if((n==k) && (m==g))              //判断两个矩阵是否行数和列数相同，不相同则不能运算 
-       {
-       	k=n;                          //使他们两个的函数和列数各为同一个变量 
-       	g=m;
-	   
-    	menu();                         //输出菜单函数，供用户使用 
-        printf("请输入你的选择：\n");
-	    int x;                      
-        scanf("%d",&x);                 //输入选择 
-        switch (x)
+	input(n,m,a,b);
+    menu();                         //输出菜单函数，供用户使用 
+    printf("请输入你的选择：\n");
+	int x;                      
+    scanf("%d",&x);                 //输入选择 
+    switch (x)
         {
-			case 1:matrixadd(a,b,n,m);               //引用加法函数 
+			case 1:matrixadd(n,m,a,b);               //引用加法函数 
 					break;
-			case 2:matrixsubtract(a,b,n,m);         //引用减法函数 
+			case 2:matrixsubtract(n,m,a,b);         //引用减法函数 
 					break;
-			case 3:matrixmultiply(a,n,m);          //引用数乘函数 
+			case 3:matrixmultiply(n,m,a);          //引用数乘函数 
 			        break;
-			case 4:matrixtransposition(a,n,m);    //引用转置函数 
+			case 4:matrixtransposition(n,m,a);    //引用转置函数 
 		        	break;
-			case 5:matrixinverse(a,n,m);          //引用求逆函数 
+			case 5:matrixinverse(n,m,a);          //引用求逆函数 
 			        break;
 			default:printf("结束\n");   //如果是五种选择之外的则直接结束 printf("结束"); 
-	    }
-	   } 
-	else
-	{
-		printf("不能进行运算\n");
-	} 
+	    } 
 	return 0;
 }
 
