0a4625b4d4658eb4d689d926ec92390f8a5b36ac
yjwchj <61540228+yjwchj@users.noreply.github.com>
2020-03-19 11:12:40 +0800

    Delete list2.h

--- list2.h
+++
@@ -1,7 +0,0 @@
-void matrixadd(int n,int m,double (*p1)[m],double (*p2)[m]);//j矩阵的加法运算函数 
-void matrixsubtract(int n,int m,double (*p1)[m],double (*p2)[m]);//矩阵的减法运算函数 
-void matrixmultiply(int n,int m,double (*p1)[m]);//矩阵的数乘运算函数  
-void matrixtransposition(int n,int m,double (*p1)[m]);//矩阵的转置运算函数
-void matrixinverse(int n,int m,double (*p1)[m]);//矩阵的求逆运算函数 
-void menu();//菜单函数 
-void input(int n,int m,double (*p1)[m],double (*p2)[m]); 
